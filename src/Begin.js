6cd1a2c56e447e850b7ba4a474598e008e619a03
jonjperkins <jjperk@gmail.com>
2017-08-15 09:58:07 -0500

    Fix apostrophe and quote issue causing error hightlightin github repo

--- src/Begin.js
+++ src/Begin.js
@@ -9,8 +9,8 @@ class Begin extends Component {
 						<div className="wizard-inner">
 							<div className="wizard-content">
 								<h1>Welcome to the LeadConduit Getting Started Guide.</h1>
-								<h4>We'll begin by asking one or two questions so we can customize your experience.</h4>
-								<button className="wizard-yes-button" style={{ textDecoration: 'none' }} onClick={this.props.handleBegin}>Begin</button>
+								<h4>We’ll begin by asking one or two questions so we can customize your experience.</h4>
+								<button className="wizard-yes-button" style={{ textDecoration: "none" }} onClick={this.props.handleBegin}>Begin</button>
 							</div>
 						</div>
 					</div>
