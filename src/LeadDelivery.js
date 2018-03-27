0dd4b6c13c28ffdb34df6b28f34a6c2e235c55f7
Benjamin Martin <benjamin.martin2010@gmail.com>
2018-03-27 13:35:42 -0500

    Update with new recipient info

--- src/LeadDelivery.js
+++ src/LeadDelivery.js
@@ -38,17 +38,17 @@ class LeadDelivery extends Component {
 					In this guide, we'll cover delivering our leads by:</p>
 					<ul>
 						<li>Email Delivery</li>
-						<li>Rich Integration Delivery</li>
-						<li>Standard Integration Delivery</li>
+						<li>Rich Integration Recipient</li>
+						<li>Standard Integration Recipient</li>
 					</ul>
-					<p>There are many delivery options in LeadConduit; <a href="https://support.activeprospect.com/hc/en-us/categories/200350369-LeadConduit" target="_blank">
+					<p>There are many recipient options in LeadConduit; <a href="https://support.activeprospect.com/hc/en-us/categories/200350369-LeadConduit" target="_blank">
 					take a look</a> to see additional outbound integrations 
-					you might use, including batch file delivery and more advanced delivery formatting options.</p>
+					you might use, including batch file recipient and more advanced recipient formatting options.</p>
 					<br/>
 					<h2>Email Delivery</h2>
-					<p>Delivering leads to your email address is a fairly easy way to get started. On the <strong>Steps</strong> page (where you added enhancements earlier in the guide), click the <strong>Add Delivery</strong> button. 
+					<p>Delivering leads to your email address is a fairly easy way to get started. On the <strong>Steps</strong> page (where you added enhancements earlier in the guide), click the <strong>Add Recipient</strong> button. 
 					Scroll down and select the <strong>Email Delivery</strong> option. This will add an email delivery step to your flow.</p>
-					<p data-tip="Click to restart"><img onClick={this.resetGif.bind(this)} className="gif-background" src={this.state.gifShowing ? require('../images/email_delivery.gif') : require('../images/email_deliveryfalse.gif')} alt="Email Delivery" /></p>
+					<p data-tip="Click to restart"><img onClick={this.resetGif.bind(this)} className="gif-background" src={this.state.gifShowing ? require('../images/new_email_delivery.gif') : require('../images/new_email_deliveryfalse.gif')} alt="Email Delivery" /></p>
 					<ReactTooltip place="bottom" type="dark" effect="float"/>
 					<p>Select the <strong>Edit Field Mappings</strong> button on this step and enter the email address where you'd like the leads delivered. Comma-separate multiple addresses if 
 					more than one person wants to receive these leads. If you add another mapping, you'll be able to further configure your email address with things
@@ -57,31 +57,31 @@ class LeadDelivery extends Component {
 					<p>As long as you provide an email address, LeadConduit will send qualified leads to that address with all of the information 
 					collected during the lead’s journey through the flow.</p>
 					<br/>
-					<h2>Rich Integration Deliveries</h2>
+					<h2>Rich Integration Recipients</h2>
 					<p>At ActiveProspect, we strive to make getting leads from one place to another as easy as possible for our clients. 
 					Not only do we have a massive list of integrations with places where our clients send leads, we’ve recently rolled out 
 					rich user interface integrations for a few of our delivery destinations. These <strong>rich integrations</strong> make sending leads 
 					from your flow your delivery destination a couple steps easier to set up. In this guide, we’ll show you how to use the 
 					MailChimp integration. </p>
-					<p>On your flow’s <strong>step editor page</strong>, select <strong>Add Delivery</strong> at the bottom right of the page. 
-					Scroll through the list of integrated delivery destinations, select <strong>MailChimp</strong>, and click <strong>OK</strong>. 
+					<p>On your flow’s <strong>step editor page</strong>, select <strong>Add Recipient</strong>. 
+					Select <strong>create a new custom recipient</strong>, search for and select <strong>MailChimp</strong>, <strong>name</strong> your recipient and click <strong>Next</strong>. 
 					You’ll be taken to a screen that asks for your MailChimp API key. If you aren’t sure what that is or where to find it, click 
 					on the <strong>How do I find my API key?</strong> link. Once you have your API key, paste it into the input box and click <strong>Next</strong>.</p>
-					<p data-tip="Click to restart"><img onClick={this.resetGif.bind(this)} className="gif-background" src={this.state.gifShowing ? require('../images/addmailchimp.gif') : require('../images/addmailchimpfalse.gif')} alt="Add MailChimp" /></p>
+					<p data-tip="Click to restart"><img onClick={this.resetGif.bind(this)} className="gif-background" src={this.state.gifShowing ? require('../images/new_addmailchimp.gif') : require('../images/new_addmailchimpfalse.gif')} alt="Add MailChimp" /></p>
 					<ReactTooltip place="bottom" type="dark" effect="float"/>
 					<p>You’ll then be prompted to select from a group of mailing lists associated with your account. Select the proper list and click <strong>OK</strong>.</p>
 					<img className="img-background" src={require('../images/selectmailchimplist.png')} alt="Select MailChimp List" />
-					<p>The MailChimp delivery is now set up and ready to add email addresses to your list! If you’re also collecting data like <strong>First Name</strong>, 
-					<strong>Last Name</strong>, or <strong>IP Address</strong>, the MailChimp delivery integration will automatically send those values as well (as long as you’re using the 
+					<p>The MailChimp recipient is now set up and ready to add email addresses to your list! If you’re also collecting data like <strong>First Name</strong>, 
+					<strong>Last Name</strong>, or <strong>IP Address</strong>, the MailChimp recipient integration will automatically send those values as well (as long as you’re using the 
 					standard fields associated with this data). If you wanted to send other data to your list (e.g. mailchimp_status), you 
 					could perform some outbound field mappings to send this data as long as it’s on the list of
 					<a href="https://support.activeprospect.com/hc/en-us/articles/115002220163" target="_blank"> fields MailChimp accepts</a>.
 					You can learn more about <strong>outbound field mapping</strong> in the next section of this guide.</p>
-					<p>Lastly, you’ll want to add a filter after this MailChimp delivery. If your delivery attempt to MailChimp is unsuccessful, 
-					you’ll need a way to be alerted to that fact. Without a filter checking to see if the MailChimp delivery was successful, all 
+					<p>Lastly, you’ll want to add a filter after this MailChimp step. If your recipient attempt to MailChimp is unsuccessful, 
+					you’ll need a way to be alerted to that fact. Without a filter checking to see if the MailChimp recipient was successful, all 
 					of your leads will appear successful in the LeadConduit user interface. Further, if you want to check out all of your events 
 					or report on good and bad leads, you need filters marking these leads appropriately. </p>
-					<p>Add a filter after the MailChimp step that will reject the lead and stop the flow if the MailChimp delivery outcome <strong><em>is not equal to</em> Success</strong>. </p>
+					<p>Add a filter after the MailChimp step that will reject the lead and stop the flow if the MailChimp recipient outcome <strong><em>is not equal to</em> Success</strong>. </p>
 					<p data-tip="Click to restart"><img onClick={this.resetGif.bind(this)} className="gif-background" src={this.state.gifShowing ? require('../images/mailchimpfilter.gif') : require('../images/mailchimpfilterfalse.gif')} alt="MailChimp Filter" /></p>
 					<ReactTooltip place="bottom" type="dark" effect="float"/>
 					<p>This is how your final filter should look:</p>
@@ -89,20 +89,20 @@ class LeadDelivery extends Component {
 					<p>Now when a lead enters your flow and isn’t successfully added to the relevant MailChimp list, you’ll be notified visually on 
 					your <strong>dashboard</strong>, and the <strong>events page</strong>.</p>
 					<br/>
-					<h2>Standard Integration Deliveries</h2>
-					<p>Standard integration deliveries ultimately have the same requirements as their <strong>Rich Integration</strong> counterparts, but you’ll 
+					<h2>Standard Integration Recipients</h2>
+					<p>Standard integration recipients ultimately have the same requirements as their <strong>Rich Integration</strong> counterparts, but you’ll 
 					need to open the step itself and configure this information manually.</p>
-					<p>To configure a standard integration delivery, click the <strong>Add Delivery</strong> button in the <strong>flow editor</strong>, 
-					and select an standard integration like Velocify. Once you make this selection, the step will be added to the end of your flow. 
-					Select your integration and click the <strong>Edit Field Mappings</strong> button. If for some reason you don’t see these options, 
+					<p>To configure a standard integration recipient, click the <strong>Add Recipient</strong> button in the <strong>flow editor</strong>, 
+					and search for and select a standard integration like Velocify. Once you make this selection, you'll be able to name the recipient and the recipient will be added to the end of your flow. 
+					Select your integration, if necessary, and click the <strong>Edit Field Mappings</strong> button. If for some reason you don’t see these options, 
 					expand the step options by clicking on its gray header.</p>
 					<img className="img-background" src={require('../images/velocifyfields.png')} alt="Velocify Fields" />
-					<p>With each standard integration, you’ll need to find the relevant required information associated with the delivery 
-					destination. For example, Velocify deliveries require that you find and map your <strong>Velocify Client</strong> and 
-					<strong>Velocify Campaign ID</strong>.</p>
+					<p>With each standard integration, you’ll need to find the relevant required information associated with the recipient 
+					destination. For example, Velocify recipients require that you find and map your <strong>Velocify Client</strong> and 
+					<strong> Velocify Campaign ID</strong>.</p>
 					<img className="img-background" src={require('../images/requiredvelocifyfields.png')} alt="Required Velocify Fields" />
 					<p>Remember to <a href="https://support.activeprospect.com/hc/en-us/articles/115001382483-LeadConduit-Filters" target="_blank">add a filter </a>
-					after this delivery step.</p>
+					after this recipient step.</p>
 					<img className="img-background" src={require('../images/velocifyfilter.png')} alt="Velocify Filter" />
 
 
@@ -124,7 +124,7 @@ class LeadDelivery extends Component {
 					learning purposes: <strong>http://www.mocky.io/v2/5900f11b1200000317c7b71f</strong>.</p>
 					<p>To get started click the <strong>Add a Delivery</strong> button. Scroll to the very bottom and create a 
 					<strong>custom delivery</strong>. Give it a name, select a type of <strong>CRM</strong> and click <strong>Save and add to flow</strong>.</p>
-					<p data-tip="Click to restart"><img onClick={this.resetGif.bind(this)} className="gif-background" src={this.state.gifShowing ? require('../images/createcustomdelivery.gif') : require('../images/createcustomdeliveryfalse.gif')} alt="Custom Delivery" /></p>
+					<p data-tip="Click to restart"><img onClick={this.resetGif.bind(this)} className="gif-background" src={this.state.gifShowing ? require('../images/new_createcustomdelivery.gif') : require('../images/new_createcustomdeliveryfalse.gif')} alt="Custom Delivery" /></p>
 					<p>Next, you need to select your integration. This part might seem tricky if you don’t have much experience with HTTP. 
 					But not to worry, there are a finite amount of ways to send data to an endpoint, and we've covered each of them in our 
 					<a href="https://support.activeprospect.com/hc/en-us/sections/201013939-LeadConduit-Flows" target="_blank"> LeadConduit knowledge base</a>.</p>
